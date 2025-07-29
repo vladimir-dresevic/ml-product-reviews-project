f46b9bd6e2bb49c6ef3c9e602b150a572ac867ae
vladimir-dresevic <vladimir.dresevic@gmail.com>
2025-07-30 00:34:57 +0200

    train_model.py added and .gitignore

---
+++ src/train_model.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+df = pd.read_csv("data/product_reviews_full.csv")
+
+# Drop all rows with missing values
+df = df.dropna()
+
+# Convert all sentiment values to lowercase and strip extra spaces
+df['sentiment'] = df['sentiment'].astype(str).str.lower().str.strip()
+
+
+# Convert column type to 'category'
+df['sentiment'] = df['sentiment'].astype('category')
+
+# Drop columns that are not useful for modeling
+df = df.drop(columns=['review_uuid', 'product_name', 'product_price'])
+
+
+# Create new column with length of each review_text
+df['review_length'] = df['review_text'].astype(str).str.len()
+
+
+# Features and label
+X = df[["review_title", "review_text", "review_length"]]
+y = df["sentiment"]
+
+# Preprocessor: TF-IDF for text, MinMaxScaler for numeric feature
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("title", TfidfVectorizer(), "review_title"),
+        ("text", TfidfVectorizer(), "review_text"),
+        ("length", MinMaxScaler(), ["review_length"])
+    ]
+)
+
+pipeline = Pipeline([
+    ("preprocessing", preprocessor),
+    ("classifier", RandomForestClassifier)
+])
+
+# Train the model on the entire dataset
+pipeline.fit(X, y)
+
+# Save the model to a file
+joblib.dump(pipeline, "model/sentiment_model.pkl")
+
+print("✅ Model trained and saved as 'model/sentiment_model.pkl'")
+
